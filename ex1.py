054ac0559b05d875f888ceef8138a41b0f14eed9
Margaret Kelly Baramuli <22149904+maggiebzt@users.noreply.github.com>
2018-07-10 16:44:27 -0700

    finished ex1 and study drill

---
+++ ex1.py
@@ -0,0 +1,17 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+#-------------------------
+# Study Drill
+
+# 1. 
+print "Make my script print another line"
+
+# 2. Put '#' in front of all lines except for one line
+
+# 3. Comment out a code line. Python will skip comments when run.
