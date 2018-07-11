5548762a59ce0ee55e2bada556659a6b5ea10fe5
Margaret Kelly Baramuli <22149904+maggiebzt@users.noreply.github.com>
2018-07-10 18:14:14 -0700

    finished ex07 and study drill

---
+++ ex07.py
@@ -0,0 +1,19 @@
+# Print out strings
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that mary went."
+print "." * 10 # Prints "." 10 times
+
+# Variables
+end1 = "a"
+end2 = 'b'
+end3 = 'c'
+
+# Print out "a bc"
+print end1,
+print end2 + end3
+
+# Print out "a
+# bc"
+print end1
+print end2 + end3
