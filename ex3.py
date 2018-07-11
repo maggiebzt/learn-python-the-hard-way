2fb2f9ca336e7525588558facdb1ee74898ce552
Margaret Kelly Baramuli <22149904+maggiebzt@users.noreply.github.com>
2018-07-10 17:10:21 -0700

    finished ex3 and study drill

---
+++ ex3.py
@@ -0,0 +1,100 @@
+# Print out sentence
+print "I will now count my chickens:"
+
+# Print out sentence and calculation
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+# Print out sentence
+print "Now I will count the eggs:"
+
+# Print out calculation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Print out sentence
+print "Is it true that 3 + 2 < 5 - 7 ?"
+
+# Print out 'True' or 'False' 
+print 3 + 2 < 5 - 7 # False
+
+# Print out sentences and calculations
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# Print out sentences
+print "Oh that's why it's False.'"
+
+print "How about some more."
+
+# Print out sentences and 'True's or 'False's
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= 2
+
+print '------------------'
+print 'Study Drill'
+
+# 3. 
+# Use '+' plus
+print "12345 + 6789 =", 12345 + 6789
+
+# Use '-' minus
+print "12345 - 6789 =", 12345 - 6789
+
+# Use '/' slash
+print "12345 / 5 =", 12345 / 5
+
+# Use '*' asterisk
+print "123 * 45 =", 123 * 45
+
+# Use '%' percent
+print "123 % 45 =", 123 % 45
+
+# Use '<' less-than
+print 'Is 123 < 45?', 123 < 45
+
+# Use '>' greater-than
+print 'Is 123 > 45?', 123 > 45
+
+# Use '<=' less-than-equal
+print 'Is 123 <= 45?', 123 <= 45
+
+# Use '>=' greater-than-equal
+print 'Is 123 >= 45?', 123 >= 45
+
+# 4. Floating point number
+# Represent real numbers, written in a decimal point
+
+# 5.
+# Print out sentence
+print "I will now count my chickens:"
+
+# Print out sentence and calculation
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# Print out sentence
+print "Now I will count the eggs:"
+
+# Print out calculation
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Print out sentence
+print "Is it true that 3 + 2 < 5 - 7 ?"
+
+# Print out 'True' or 'False' 
+print 3.0 + 2.0 < 5.0 - 7.0 # False
+
+# Print out sentences and calculations
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+# Print out sentences
+print "Oh that's why it's False.'"
+
+print "How about some more."
+
+# Print out sentences and 'True's or 'False's
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= 2.0
