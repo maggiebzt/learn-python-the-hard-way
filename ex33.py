7ccf8773596df02175eb8912e9f28b8ee5e79481
Margaret Kelly Baramuli <22149904+maggiebzt@users.noreply.github.com>
2018-07-12 12:45:08 -0700

    avoided printing "None" in output "

--- ex33.py
+++ ex33.py
@@ -20,22 +20,28 @@ for num in numbers:
 # 1. 
 def print_while_loop(int):
 	a = 0
+	string = ""
 	while a < int:
-		print "a is %d" % a
+		string += "a is %d\n" % a
 		numbers.append(a)
 		a += 1
+	return string
 
 # 2.
-print "int = 0\n", print_while_loop(0) # Print "None". I expected it to print just nothing
+print "int = 0\n", print_while_loop(0) 
+# Print "None". I expected it to print just nothing
+# https://stackoverflow.com/questions/28812851/why-is-this-printing-none-in-the-output
 print "int = 1\n", print_while_loop(1)
 print "int = 10\n", print_while_loop(10)
 
 # 3.
 def print_while_loop2(int, increment):
 	a = 0
+	string = ""
 	while a < int:
-		print "a is %d" % a
+		string += "a is %d\n" % a
 		numbers.append(a)
 		a += increment
+	return string
 	
 print "int = 11, increment = 5\n", print_while_loop2(11, 5) # 0, 5, 10
