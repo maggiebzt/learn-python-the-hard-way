7e127663c63b8da8d1bc1ca484dea7d3be9611c1
Margaret Kelly Baramuli <22149904+maggiebzt@users.noreply.github.com>
2018-08-06 14:50:43 -0700

    added comments

--- ex44a.py
+++ ex44a.py
@@ -1,13 +1,20 @@
+# Create a class Parent
 class Parent(object):
 	
+	# Method of class Parent which takes itself as a parameter
 	def implicit(self):
 		print "PARENT implicit()"
 
+# Create a class Child which inherits Parent
 class Child(Parent):
 	pass
 
+# Create a Parent object and call it dad
 dad = Parent()
+
+# Create a Child object and call it son
 son = Child()
 
+# Call on the method implicit
 dad.implicit()
 son.implicit()
